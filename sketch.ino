9c0bc9b9d1059f636ea8d57f437b0e2ebfc734a9
Sudipto Ghosh <sudipto@ghosh.pro>
2019-06-19 20:56:01 +0530

    Added Tone Generator

--- sketch.ino
+++ sketch.ino
@@ -35,7 +35,7 @@ void loop()
 /**
  * Analogue IO
  */
-
+/*
 const int sensorPin = A0;
 const int outputPin = 9;
 
@@ -55,4 +55,38 @@ void loop()
   Serial.println("Sensor: " + String(sensorValue) + " / 1023");
   Serial.println("Output: " + String(outputValue) + " / 255");
   delay(1);
+}
+*/
+
+/**
+ * Tone Generator
+ */
+
+#include "lib/pitches.h"
+
+int melody[] = {
+  NOTE_DS3, NOTE_FS3, NOTE_AS3, NOTE_AS3, NOTE_AS3, NOTE_B3, 0, 
+  NOTE_B3, NOTE_AS3, NOTE_AS3, NOTE_B3, NOTE_B3, NOTE_AS3, NOTE_GS3
+};
+
+int noteDurations[] = {
+  8, 8, 4, 4, 4, 8, 2, 
+  4, 8, 8, 8, 4, 4, 8
+};
+
+void setup() {
+  for (int thisNote = 0; thisNote < 14; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(9, melody[thisNote], noteDuration);
+    
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+    noTone(9);
+  }
+}
+
+void loop() {
+  setup();
+  delay(5000);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 }
